acd1e00e94e14e7015c5c5d954ab849174be8bdf
Rinkikumari19 <rinki19@navgurukul.org>
2021-01-21 00:54:58 +0530

    use eye icon in form

---
+++ js/form.js
@@ -0,0 +1,14 @@
+const pwdvisibility = document.querySelector('.visibility');
+const pwdtype = document.querySelector('.pass input');
+
+var password = true;
+pwdvisibility.addEventListener('click', function () {
+    if (password){
+        pwdtype.setAttribute('type', 'text');
+        pwdvisibility.innerHTML = 'visibility';
+    } else {
+        pwdtype.setAttribute('type', 'password'); 
+        pwdvisibility.innerHTML = 'visibility_off';
+    }
+    password =! password;
+});
